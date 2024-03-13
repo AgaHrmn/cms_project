0ed015140254e4c1bdfc27c1ba3cc54b2e391354
agaolejniq <a.olejnikk@gmail.com>
2024-03-13 15:25:04 +0100

    project setup

---
+++ cms/src/main/java/fantastic/cms/CmsApplication.java
@@ -0,0 +1,13 @@
+package fantastic.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsApplication.class, args);
+	}
+
+}
